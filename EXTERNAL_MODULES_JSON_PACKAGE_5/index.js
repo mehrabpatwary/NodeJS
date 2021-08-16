6e354285e5bac0a3fdf8ca5c1d5ebb6780d274eb
Mehrab Patwary <39481182+mehrabpatwary@users.noreply.github.com>
2021-08-16 17:35:07 +0600

    Add files via upload

---
+++ EXTERNAL_MODULES_JSON_PACKAGE_5/index.js
@@ -0,0 +1,8 @@
+const getRendomFruitsNames = require('random-fruits-name');
+const randomMovieNames = require('random-movie-names');
+
+console.log(getRendomFruitsNames('en'));
+console.log(randomMovieNames());
+
+// command: npm init
+// For install npm
