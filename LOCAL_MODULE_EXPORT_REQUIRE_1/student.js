b66b90994d0025633c1370821913cb591601beb9
Mehrab Patwary <39481182+mehrabpatwary@users.noreply.github.com>
2021-08-16 12:24:05 +0600

    Add files via upload

---
+++ LOCAL_MODULE_EXPORT_REQUIRE_1/student.js
@@ -0,0 +1,16 @@
+// const getName = () => {
+//     return "Mehrab Patwary";
+// }
+exports.getName = () => {
+    return "Mehrab Patwary";
+}
+exports.getAge = () => {
+    return "25";
+}
+exports.cgpa = 3.62;
+
+// exports.getName = getName;
+
+// module.exports = {
+//     getName,
+// }
