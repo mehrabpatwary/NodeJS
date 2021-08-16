a93f9cc80987e526788e1ec20a742834ba3f7b58
Mehrab Patwary <39481182+mehrabpatwary@users.noreply.github.com>
2021-08-16 13:53:43 +0600

    Add files via upload

--- HTTP_STSTUS_CODE_4/server.js
+++ HTTP_STSTUS_CODE_4/server.js
@@ -4,11 +4,11 @@ const hostname = '127.0.0.1';
 
 const myserver = http.createServer((req, res) => {
     res.writeHead(202, {'Content-Type':'text/html'});
-    res.write("<h1>Hi Bunny</h1>");
-    res.write("<h1>Hi Bunny</h1>");
-    res.write("<h1>Hi Bunny</h1>");
-    res.write("<h1>Hi Bunny</h1>");
-    res.write("<h1>Hi Bunny</h1>");
+    res.write("<h1>Hello world</h1>");
+    res.write("<h1>Hello world</h1>");
+    res.write("<h1>Hello world</h1>");
+    res.write("<h1>Hello world</h1>");
+    res.write("<h1>Hello world</h1>");
     res.end();
 });
 
