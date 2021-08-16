3146715870d414ecd51cdc65a5beace966c28aac
Mehrab Patwary <39481182+mehrabpatwary@users.noreply.github.com>
2021-08-16 13:54:41 +0600

    Add files via upload

--- HTTP_STSTUS_CODE_4/server.js
+++ HTTP_STSTUS_CODE_4/server.js
@@ -1,17 +1,17 @@
-const http = require('http');
-const port = 3000;
-const hostname = '127.0.0.1';
+// const http = require('http');
+// const port = 3000;
+// const hostname = '127.0.0.1';
 
-const myserver = http.createServer((req, res) => {
-    res.writeHead(202, {'Content-Type':'text/html'});
-    res.write("<h1>Hello world</h1>");
-    res.write("<h1>Hello world</h1>");
-    res.write("<h1>Hello world</h1>");
-    res.write("<h1>Hello world</h1>");
-    res.write("<h1>Hello world</h1>");
-    res.end();
-});
+// const myserver = http.createServer((req, res) => {
+//     res.writeHead(202, {'Content-Type':'text/html'});
+//     res.write("<h1>Hello world</h1>");
+//     res.write("<h1>Hello world</h1>");
+//     res.write("<h1>Hello world</h1>");
+//     res.write("<h1>Hello world</h1>");
+//     res.write("<h1>Hello world</h1>");
+//     res.end();
+// });
 
-myserver.listen(port, hostname, ()=> {
-    console.log(`Your server is successfully running on http://${hostname}:${port}`);
-})
+// myserver.listen(port, hostname, ()=> {
+//     console.log(`Your server is successfully running on http://${hostname}:${port}`);
+// });
